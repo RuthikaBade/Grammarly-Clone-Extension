de183858be0a7a11c57f93d122370402ff8c5d67
RuthikaBade <ruthikabade@gmail.com>
2025-10-30 20:10:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello from FastAPI!"}
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/check")
+def check_grammar(input_text: TextInput):
+    original = input_text.text
+    suggestion = original.replace(" .", ".")
+    if suggestion == original:
+        explanation = "No major issues found."
+    else:
+        explanation = "Removed unnecessary space before period."
+    return {
+        "original": original,
+        "suggestion": suggestion,
+        "explanation": explanation
+    }
